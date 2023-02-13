e133b3ce566952a062e87951eab8dac9f3e6b148
Amari Bauer <baueramari@cs.uchicago.edu>
2023-02-12 18:16:33 -0600

    Restructuring/renaming

--- raw_data/crime_api.py
+++ raw_data/crime_api.py
@@ -4,10 +4,8 @@ from sodapy import Socrata
 client = Socrata('data.cityofchicago.org',
                   'thcdLYLxkemKtnhH7zQBnZAc9')
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
 results = client.get("ijzp-q8t2", limit=2000)
+#Eliminate limit when doing full-scale analyses
 
-# Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 results_df.to_csv('crimes.csv')
