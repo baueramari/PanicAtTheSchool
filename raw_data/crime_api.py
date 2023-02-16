5b05fe28e76a2697ecda8bce028a4b6e253ec9c3
Amari Bauer <baueramari@cs.uchicago.edu>
2023-02-16 15:44:14 -0600

    API confirmed

--- raw_data/crime_api.py
+++ raw_data/crime_api.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.environ['TOKEN']
- #need to figure this out for security.
+
 client = Socrata('data.cityofchicago.org',
                   TOKEN, timeout = 1200)
 
-#results = client.get("ijzp-q8t2", limit = 8000000)
+results = client.get("ijzp-q8t2", limit = 8000000)
 
-#results_df = pd.DataFrame.from_records(results)
-#results_df.to_csv('raw_data/crimes.csv')
+results_df = pd.DataFrame.from_records(results)
+results_df.to_csv('raw_data/crimes.csv')
 
