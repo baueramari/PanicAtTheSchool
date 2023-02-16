893ff1cdd80504afad425f8a308e745de59a7137
Amari Bauer <baueramari@cs.uchicago.edu>
2023-02-16 15:09:45 -0600

    API - token private

--- raw_data/crime_api.py
+++ raw_data/crime_api.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sodapy import Socrata
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = os.environ['TOKEN']
-if TOKEN:
-    print('BACON') #need to figure this out for security.
-#client = Socrata('data.cityofchicago.org',
-#                  '', timeout = 1200)
+ #need to figure this out for security.
+client = Socrata('data.cityofchicago.org',
+                  TOKEN, timeout = 1200)
 
-##results = client.get("ijzp-q8t2", limit = 8000000)
+#results = client.get("ijzp-q8t2", limit = 8000000)
 
 #results_df = pd.DataFrame.from_records(results)
 #results_df.to_csv('raw_data/crimes.csv')
