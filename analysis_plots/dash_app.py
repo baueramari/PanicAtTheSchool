ed8494ccd958ef9d62e29f32ff68fc41b714df0b
Amari Bauer <baueramari@cs.uchicago.edu>
2023-02-23 11:07:34 -0600

    OSS plot added to interface

--- analysis_plots/dash_app.py
+++ analysis_plots/dash_app.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
-from analysis_plots.plots import plot_crime
+from analysis_plots.plots import plot_crime, scatter_OSS_attendance
 
 app = dash.Dash()
 # VERY INITIAL STAGE USER INTERFACE
@@ -22,12 +22,12 @@ app.layout = html.Div(
         dcc.Graph(id="line_plot", figure=plot_crime()),
         html.H2(
             id="H2",
-            children="School Suspension in Crime-Varying Areas",
+            children="Out of School Suspension and Attendance",
             style={"textAlign": "center", "marginTop": 40, "marginBottom": 40},
         ),
-        dcc.Graph(id="line_plot", figure=plot_crime()),
+        dcc.Graph(id="line_plot", figure=scatter_OSS_attendance()),
     ],
 )
 
 if __name__ == "__main__":
-    app.run_server(port=5001)
+    app.run_server(port=6001)
