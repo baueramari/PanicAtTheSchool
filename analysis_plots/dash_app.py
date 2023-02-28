552fd878f5dd426a3adc14055c9f28d3029acac6
Amari Bauer <baueramari@cs.uchicago.edu>
2023-02-28 12:45:12 -0600

    Callback adjustment, not working

--- analysis_plots/dash_app.py
+++ analysis_plots/dash_app.py
@@ -28,7 +28,7 @@ app.layout = html.Div(
             ],
             value="project_intro",
         ),
-        html.Div(dcc.Textarea(id="Text Area", value="project_intro")),
+        html.Div(dcc.Textarea(id="Text Area", value={})),
     ],
 )
 
@@ -38,20 +38,21 @@ app.layout = html.Div(
     [Input(component_id="dropdown", component_property="value")],
 )
 def project_intro(dropdown):
-    text = dcc.Textarea(
-        id="Text Area",
-        value="""CAPP 30122 Group Project: Sarah, Eshan, and Amari
+    if dropdown == "Project Introduction":
+        text = dcc.Textarea(
+            id="Text Area",
+            value="""CAPP 30122 Group Project: Sarah, Eshan, and Amari
 
-TLDR: We have moved away from looking into enrollment and learning gains, and now are focusing only at attendance drops. Our goal is to investigate reasons for drops (read below) and visualize our findings on a web app.
+    TLDR: We have moved away from looking into enrollment and learning gains, and now are focusing only at attendance drops. Our goal is to investigate reasons for drops (read below) and visualize our findings on a web app.
 
-Our aim is to assess the drop in attendance observed by Chicago Public Schools in the last decade, but more specifically, in the time returning from COVID. Based on attendance data from CPS, pre-K and grades 9-12 have shown the most noticeable drop in attendance. For these grades, in 2018 and 2019, for example, the average attendance was 86.15% and 86.51% respectively. Post Covid, these numbers dropped to 79.72% and 78.79% respectively in 2021 and 2022. News agencies covering this issue have attributed these trends to the emergence and solidification of the Chicago Teachers Union. While we’re not looking into those claims through this project, we want to investigate other variables that might have impacted attendance. Specifically, we want to look at 3 buckets: a) Neighborhood indicators: These will cover: Demographic build up of school neighborhood Socioeconomic indicators - employment, income etc. Health indicators Accessibility indicators such as walkability score, access to public transport etc.
-b) Investment in school indicators: These will cover: Per pupil budget for schools Teacher distribution by race/qualification Attendance distribution by school type c) Crime and punishment indicators: Crime in the school neighborhood Suspensions/expulsions handed out by school authorities Through our analysis, we want to see which of these factors impacts attendance the most. Finally, we will visualize our results on a web-app and write down some potential next steps based on research of news articles/interviews with CPS employees.')""",
-        style={
-            "width": "100%",
-            "height": 200,
-        },
-    )
-    return text
+    Our aim is to assess the drop in attendance observed by Chicago Public Schools in the last decade, but more specifically, in the time returning from COVID. Based on attendance data from CPS, pre-K and grades 9-12 have shown the most noticeable drop in attendance. For these grades, in 2018 and 2019, for example, the average attendance was 86.15% and 86.51% respectively. Post Covid, these numbers dropped to 79.72% and 78.79% respectively in 2021 and 2022. News agencies covering this issue have attributed these trends to the emergence and solidification of the Chicago Teachers Union. While we’re not looking into those claims through this project, we want to investigate other variables that might have impacted attendance. Specifically, we want to look at 3 buckets: a) Neighborhood indicators: These will cover: Demographic build up of school neighborhood Socioeconomic indicators - employment, income etc. Health indicators Accessibility indicators such as walkability score, access to public transport etc.
+    b) Investment in school indicators: These will cover: Per pupil budget for schools Teacher distribution by race/qualification Attendance distribution by school type c) Crime and punishment indicators: Crime in the school neighborhood Suspensions/expulsions handed out by school authorities Through our analysis, we want to see which of these factors impacts attendance the most. Finally, we will visualize our results on a web-app and write down some potential next steps based on research of news articles/interviews with CPS employees.')""",
+            style={
+                "width": "100%",
+                "height": 200,
+            },
+        )
+        return text
 
 
 """
@@ -78,4 +79,4 @@ def display_plots(dropdown):
 """
 
 if __name__ == "__main__":
-    app.run_server(port=6035)
+    app.run_server(port=6036)
