0e7caaae0c3675e1ffe1b1a8e7db4bda0d2a38e6
Amari Bauer <baueramari@cs.uchicago.edu>
2023-03-03 09:47:47 -0600

    Path fixing

--- CAPP_project/data_wrangling/merge_data.py
+++ CAPP_project/data_wrangling/merge_data.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 import pandas as pd
+
 cwd = Path.cwd()
 parent_dir = cwd.parent
 
-avg_attend = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/avg_attend.csv")
-crime_by_ward = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/crime_by_ward.csv")
-schoolid_ward_map = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/schoolid_ward_map.csv")
+avg_attend = pd.read_csv("CAPP_project/data_wrangling/cleaned_data/avg_attend.csv")
+crime_by_ward = pd.read_csv(
+    "CAPP_project/data_wrangling/cleaned_data/crime_by_ward.csv"
+)
+schoolid_ward_map = pd.read_csv(
+    "CAPP_project/data_wrangling/cleaned_data/schoolid_ward_map.csv"
+)
 
-low_crime = crime_by_ward["crime_capita"].quantile(0.25)
-medium_crime = crime_by_ward["crime_capita"].quantile(0.75)
+low_crime = crime_by_ward["crime_capita"].quantile(0.33)
+medium_crime = crime_by_ward["crime_capita"].quantile(0.67)
 high_crime = float("inf")
 crime_by_ward["crime_class"] = pd.cut(
     crime_by_ward["crime_capita"],
@@ -44,21 +49,23 @@ attend_by_crime = attend_id_crime.groupby(
 ).mean("Attendance")
 attend_by_crime = attend_by_crime[["crime_class", "Year", "Attendance"]]
 
-attend_id_crime.to_csv(parent_dir/"data_wrangling/merged_data/attend_id_crime.csv")
-attend_by_crime.to_csv(parent_dir/"data_wrangling/merged_data/attend_by_crime.csv")
+attend_id_crime.to_csv("CAPP_project/data_wrangling/merged_data/attend_id_crime.csv")
+attend_by_crime.to_csv("CAPP_project/data_wrangling/merged_data/attend_by_crime.csv")
 
 
-# Sarah's data merge needed for her visuals
+# Sarah's data merge needed for her visuals CAPP_project/data_wrangling/merge_data.py
 
 crime_cols = [
     "School_ID",
     "crime_class",
     "Attendance",
 ]
-ID_crimeclass = pd.read_csv(
-    parent_dir/"data_wrangling/merged_data/attend_id_crime.csv", usecols=crime_cols
+ID_crimeclass = pd.read_csv(  # this csv is already loaded in, should be good to use it from above, switch column names and be good to go
+    parent_dir / "data_wrangling/merged_data/attend_id_crime.csv", usecols=crime_cols
 )
-suspensions = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/suspension_data.csv")
+suspensions = pd.read_csv(
+    parent_dir / "data_wrangling/cleaned_data/suspension_data.csv"
+)
 
 high_schools = suspensions["School ID"].unique().tolist()
 ID_crimeclass = ID_crimeclass[ID_crimeclass["School_ID"].isin(high_schools)]
@@ -76,7 +83,9 @@ avg_suspension_crime = suspension_crime_merge.groupby(
     by="crime_class", as_index=False
 ).mean(numeric_only=True)
 avg_suspension_crime.drop("School ID", axis=1, inplace=True)
-avg_suspension_crime.to_csv(parent_dir/"data_wrangling/merged_data/avg_suspension_crime.csv")
+avg_suspension_crime.to_csv(
+    parent_dir / "data_wrangling/merged_data/avg_suspension_crime.csv"
+)
 
 # merge csv to compare suspensions and attendance
 suspension_attend_merge = pd.merge(
@@ -91,15 +100,21 @@ avg_suspension_attend = suspension_attend_merge.groupby(
     by="School ID", as_index=False
 ).mean(numeric_only=True)
 
-avg_suspension_attend.to_csv(parent_dir/"data_wrangling/merged_data/suspension_attendance.csv")
+avg_suspension_attend.to_csv(
+    parent_dir / "data_wrangling/merged_data/suspension_attendance.csv"
+)
 
 
-#Eshan's school merge/analysis code (initially bucket-2)
-#Objective is to merge all school data, then create csvs for Sarah that she can use to plot
-sch_profile = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_school_admin.csv")
-sch_att = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_attendance.csv")
-sch_finance = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_school_budget.csv")
-sch_teachers = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_teacher.csv")
+# Eshan's school merge/analysis code (initially bucket-2)
+# Objective is to merge all school data, then create csvs for Sarah that she can use to plot
+sch_profile = pd.read_csv(
+    parent_dir / "data_wrangling/cleaned_data/clean_school_admin.csv"
+)
+sch_att = pd.read_csv(parent_dir / "data_wrangling/cleaned_data/clean_attendance.csv")
+sch_finance = pd.read_csv(
+    parent_dir / "data_wrangling/cleaned_data/clean_school_budget.csv"
+)
+sch_teachers = pd.read_csv(parent_dir / "data_wrangling/cleaned_data/clean_teacher.csv")
 
 school_merged = sch_profile.merge(sch_att, left_on="sch_id", right_on="School ID")
 school_merged = school_merged.merge(
@@ -139,16 +154,18 @@ school_merged["post_att_bucket"] = school_merged.apply(
 )
 
 
-school_merged.to_csv(parent_dir/"data_wrangling/merged_data/all_school_merged.csv")
+school_merged.to_csv(parent_dir / "data_wrangling/merged_data/all_school_merged.csv")
 
-#Eshan-Other analysis pending
-#Eshan's merge/analysis of school attendance-demographic data
+# Eshan-Other analysis pending
+# Eshan's merge/analysis of school attendance-demographic data
 
-cols_to_keep = ["ca_id", "pre_cov_att","post_cov_att","att_diff_pp"]
-att_demo = school_merged.loc[:,cols_to_keep]
+cols_to_keep = ["ca_id", "pre_cov_att", "post_cov_att", "att_diff_pp"]
+att_demo = school_merged.loc[:, cols_to_keep]
 
-demog_info = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_demog.csv")
-health_info = pd.read_csv(parent_dir/"data_wrangling/cleaned_data/clean_health_atlas.csv")
+demog_info = pd.read_csv(parent_dir / "data_wrangling/cleaned_data/clean_demog.csv")
+health_info = pd.read_csv(
+    parent_dir / "data_wrangling/cleaned_data/clean_health_atlas.csv"
+)
 # Note: Check the keys in each file before merging
 # Now group data by ca_id but also take count of schools for each ca_id
 cols_grouping = {"ca_id": "count"}
@@ -161,10 +178,12 @@ att_demo = att_demo.reset_index()
 num_schools = 2
 att_demo = att_demo[att_demo["count_schools"] >= num_schools]
 
-#School data ready, now merge demographic and health data with this
+# School data ready, now merge demographic and health data with this
 merged_att_demo = pd.merge(att_demo, demog_info, on="ca_id")
 merged_att_demo = pd.merge(merged_att_demo, health_info, on="ca_id")
 cols_to_drop = ["comm_area_y"]
 merged_att_demo = merged_att_demo.drop(cols_to_drop, axis=1)
 print(merged_att_demo.columns)
-merged_att_demo.to_csv(parent_dir/"data_wrangling/merged_data/school_demo_merged.csv", index=False)
+merged_att_demo.to_csv(
+    parent_dir / "data_wrangling/merged_data/school_demo_merged.csv", index=False
+)
